03d5ea89ff6095ec736ef115feb931689b539ee6
bruhtus <hibruhtus@gmail.com>
2021-11-18 14:00:20 +0700

    feat(server.js): try out insomnia api debugging tools
    note:
    insomnia is similar to postman.

--- src/server.js
+++ src/server.js
@@ -7,9 +7,24 @@ export const app = express()
 
 app.disable('x-powered-by')
 
+// CORS stands for Cross-Origin Resource Sharing
+// CORS basically allows your API to be used by other domains
+// that are on the browser
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/data', (req, res) => {
+  res.send({ 'message': 'hello world' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('Server on port 3000')
+  })
+}
