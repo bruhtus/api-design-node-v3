6fc599619a8646eebbccd9242cdc6bdc4b7c3a67
bruhtus <hibruhtus@gmail.com>
2021-11-20 10:45:47 +0700

    feat(exercise): add my solution
    note:
    already pass the `yarn test-routes`, but there's an error when running
    `yarn dev`. need to check the course solution, i guess.

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,25 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .get((req, res) => {
+    res.send({ get: true })
+  })
+  .post((req, res) => {
+    res.send({ post: true })
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.send({ get: true })
+  })
+  .put((req, res) => {
+    res.send({ put: true })
+  })
+  .delete((req, res) => {
+    res.send({ delete: true })
+  })
+
 export default router
