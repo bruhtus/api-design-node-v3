fdc8f822682a1d0cd15d984af64a54de8519c504
bruhtus <hibruhtus@gmail.com>
2021-11-22 08:57:14 +0700

    feat(exercise): finish exercise lesson-3
    note:
    reference about schematypes:
    https://mongoosejs.com/docs/schematypes.html

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,31 @@
 import mongoose from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 50,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'active',
+    // enum is a validator that checks if the value is in the given array
+    enum: ['active', 'complete', 'pastdue'],
+  },
+  notes: String,
+  due: Date,
+  createdBy: {
+    type: mongoose.ObjectId,
+    // or type: mongoose.SchemaTypes.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  list: {
+    type: mongoose.ObjectId,
+    required: true,
+    ref: 'list',
+  },
+}, { timestamps: true })
 export const Item = mongoose.model('item', itemSchema)
