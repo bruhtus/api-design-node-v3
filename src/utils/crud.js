7ed345f6fadc0a390b7363dc62ddc3b69e3c6c53
bruhtus <hibruhtus@gmail.com>
2021-11-24 09:13:48 +0700

    feat(exercise): pass the `getOne` test
    note:
    the test is basically find the user by authenticated parameter
    (in this case is `createdBy` field) and id.
    
    if the user doesn't exist, end the request by giving status `400`.
    if the user exist, give the user data in json format and return status
    `200`.

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,4 +1,17 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+
+  //_id gonna be the same thing as the id parameters in item.router.js
+  // how did we know if they're authenticated user? they created it, there's createdBy field in it
+  const doc = await model.findOne({ _id: id, createdBy: userId }).exec()
+
+  if (!doc) {
+    return res.status(400).end()
+  }
+
+  res.status(200).json({ data: doc })
+}
 
 export const getMany = model => async (req, res) => {}
 
